c59b768dae3a12a180a821e75a32274604896ebb
Kiran Kirve <kiran2kirve@gmail.com>
2019-05-21 10:21:29 +0530

    commit first
    first commit ..

---
+++ app/src/main/java/com/kiran2kirve/kiranauto/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kiran2kirve.kiranauto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("auto","kiran auto");
+    }
+}
